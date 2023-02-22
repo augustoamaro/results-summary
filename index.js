6768c4fc470cb40a3261f7f20c4517bfdf9e1c06
Leônidas Augusto Amaro <augustoamaro@icloud.com>
2023-02-22 19:12:49 -0300

    Finished JS data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import data from './data.json' assert { type: "json" };
+
 const divs = document.querySelectorAll('.summary div');
 const colors = ['#FF5555', '#FFB21E', '#00BB8F', '#1125D6'];
 
@@ -5,4 +7,13 @@ divs.forEach((div, i) => {
     const color = colors[i];
     div.style.backgroundColor = color;
     div.style.color = color;
+
+    let icon = data[i].icon
+    div.children[0].setAttribute('src', icon)
+
+    let category = data[i].category;
+    div.children[1].textContent = category
+
+    let score = data[i].score;
+    div.children[2].textContent = score;
 });
